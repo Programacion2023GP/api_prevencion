37a65d59b2609826129587587a8f278830c87153
Luis Angel <programacion@gomezpalacio.gob.mx>
2024-02-22 15:28:54 -0600

    mapas 1

--- database/migrations/2024_02_22_195036_create_continmap_table.php
+++ database/migrations/2024_02_22_195036_create_continmap_table.php
@@ -12,6 +12,32 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('colonias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 60);
+            $table->foreignId('municipio')->constrained('municipios')->onDelete('restrict')->onUpdate('restrict');
+            $table->string('asentamiento', 40);
+            $table->integer('codigo_postal');
+            $table->decimal('latitud', 16, 13)->nullable();
+            $table->decimal('longitud', 16, 13)->nullable();
+            $table->timestamps();
+        });
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 60);
+            $table->foreignId('estado')->constrained('estados')->onDelete('restrict')->onUpdate('restrict');
+            $table->integer('cp_min');
+            $table->integer('cp_max');
+            $table->enum('huso_horario', ['Tiempo del Centro', 'Tiempo del Noroeste', 'Tiempo del Pacífico', 'Tiempo del Sureste', 'Tiempo del Centro en Frontera', 'Tiempo del Noroeste en Frontera', 'Tiempo del Pacífico en Frontera', 'Tiempo del Pacífico Sonora'])->nullable();
+            $table->timestamps();
+        });
+        Schema::create('estados', function (Blueprint $table) {
+            $table->tinyInteger('clave')->unsigned()->primary();
+            $table->string('nombre', 33);
+            $table->string('abreviacion', 4);
+            $table->integer('cp_min');
+            $table->integer('cp_max');
+        });
         DB::statement("
         CREATE OR REPLACE VIEW continmap AS
         SELECT 
@@ -44,6 +70,9 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('colonias');
+        Schema::dropIfExists('municipios');
+        Schema::dropIfExists('estados');
         Schema::dropIfExists('continmap');
     }
 };
