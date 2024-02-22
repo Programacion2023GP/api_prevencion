a8b70ed49a8164faced5c6144df114d5d09f8d11
Luis Angel <programacion@gomezpalacio.gob.mx>
2024-02-22 15:32:51 -0600

    mapas 3

--- database/migrations/2024_02_22_195036_create_continmap_table.php
+++ database/migrations/2024_02_22_195036_create_continmap_table.php
@@ -12,15 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('colonias', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre', 60);
-            $table->foreignId('municipio')->constrained('municipios');
-            $table->string('asentamiento', 40);
-            $table->integer('codigo_postal');
-            $table->decimal('latitud', 16, 13)->nullable();
-            $table->decimal('longitud', 16, 13)->nullable();
-            $table->timestamps();
+        
+        Schema::create('estados', function (Blueprint $table) {
+            $table->tinyInteger('clave')->unsigned()->primary();
+            $table->string('nombre', 33);
+            $table->string('abreviacion', 4);
+            $table->integer('cp_min');
+            $table->integer('cp_max');
         });
         Schema::create('municipios', function (Blueprint $table) {
             $table->id();
@@ -31,12 +29,15 @@ return new class extends Migration
             $table->enum('huso_horario', ['Tiempo del Centro', 'Tiempo del Noroeste', 'Tiempo del Pacífico', 'Tiempo del Sureste', 'Tiempo del Centro en Frontera', 'Tiempo del Noroeste en Frontera', 'Tiempo del Pacífico en Frontera', 'Tiempo del Pacífico Sonora'])->nullable();
             $table->timestamps();
         });
-        Schema::create('estados', function (Blueprint $table) {
-            $table->tinyInteger('clave')->unsigned()->primary();
-            $table->string('nombre', 33);
-            $table->string('abreviacion', 4);
-            $table->integer('cp_min');
-            $table->integer('cp_max');
+        Schema::create('colonias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 60);
+            $table->foreignId('municipio')->constrained('municipios');
+            $table->string('asentamiento', 40);
+            $table->integer('codigo_postal');
+            $table->decimal('latitud', 16, 13)->nullable();
+            $table->decimal('longitud', 16, 13)->nullable();
+            $table->timestamps();
         });
         DB::statement("
         CREATE OR REPLACE VIEW continmap AS
@@ -70,9 +71,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('colonias');
-        Schema::dropIfExists('municipios');
         Schema::dropIfExists('estados');
+        Schema::dropIfExists('municipios');
+        Schema::dropIfExists('colonias');
         Schema::dropIfExists('continmap');
     }
 };
